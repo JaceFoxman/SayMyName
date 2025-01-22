11b0ee53fe69f81b77b6e1c57f172692b2d48f52
Jason Permann <JaceFoxman@gmail.com>
2025-01-22 08:54:13 -0700

    Add project files.

---
+++ SayMyName/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SayMyName")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SayMyName")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("99532fef-5478-4a93-b6a0-c279da80b5d0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,17 @@
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/JaceFoxman/OperatorsExample.git
+
+'Options for this project.
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+
+    End Sub
+
+End Module
