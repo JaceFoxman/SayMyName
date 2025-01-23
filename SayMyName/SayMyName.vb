05bd3fa1a42572eb83677f35e9ff348b25753344
Jason Permann <JaceFoxman@gmail.com>
2025-01-23 16:38:00 -0700

    add dim as string and console write/read commands

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -2,7 +2,7 @@
 'Spring 2025
 'RCET2265
 'OperatorsExample
-'https://github.com/JaceFoxman/OperatorsExample.git
+'https://github.com/JaceFoxman/SayMyName.git
 
 'Options for this project.
 Option Explicit On
@@ -11,6 +11,15 @@ Option Strict On
 Module SayMyName
 
     Sub Main()
+        Dim name As String
+
+        Console.WriteLine("What is your name?")
+        name = Console.ReadLine
+        Console.WriteLine()
+
+        Console.Write(name)
+        Console.WriteLine()
+
 
     End Sub
 
